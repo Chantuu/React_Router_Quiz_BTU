80d94726c66a94b13743e75b0eb32270daba810f
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-24 03:26:51 +0400

    Project initialization

---
+++ src/components/App/App.jsx
@@ -0,0 +1,29 @@
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from 'react-router-dom';
+import './App.css';
+import MainLayout from '../../layouts/MainLayout';
+import HomePage from '../../pages/HomePage';
+import GalleryPage from '../../pages/GalleryPage';
+import ProfilePage from '../../pages/ProfilePage';
+import ErrorPage from '../../pages/ErrorPage';
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<ErrorPage />} />
+        </Route>
+    )
+);
+
+function App() {
+    return <RouterProvider router={router} />;
+}
+
+export default App;
