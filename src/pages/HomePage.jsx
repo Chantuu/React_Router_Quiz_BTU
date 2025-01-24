4f4afb1767e71b6daebf07c16ade6bcb55ee0d62
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-24 15:59:10 +0400

    Style Home Page
     Styled Home Page and made it responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,9 +7,18 @@ function HomePage() {
     const navigateToGalleryPage = () => navigate('/gallery');
 
     return (
-        <div>
-            <img src={HeroImage} alt="iPhone 12 Ad Photo" />
-            <button onClick={navigateToGalleryPage}>Go to Gallery</button>
+        <div className="h-full flex flex-col items-center justify-center gap-5 pb-16">
+            <img
+                src={HeroImage}
+                alt="iPhone 12 Ad Photo"
+                className="w-full md:w-[670px] h-auto"
+            />
+            <button
+                onClick={navigateToGalleryPage}
+                className="bg-blue-400 rounded-md text-white px-3 py-1.5 cursor-pointer transition-colors delay-75 ease-in hover:bg-blue-600"
+            >
+                Go to Gallery
+            </button>
         </div>
     );
 }
