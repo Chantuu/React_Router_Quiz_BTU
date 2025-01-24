a7361901889012a88d03ce3a460f0439bbd54453
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-24 14:37:58 +0400

    Add Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className="bg-blue-800 px-3 md:px-14 py-7">
+            <p className="text-white text-center text-sm">
+                This project is made by Giorgi Chanturia.
+                <a href="https://github.com/">
+                    Source Code will be available soon.
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
