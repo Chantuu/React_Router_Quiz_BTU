17d1808a85c6b480d071249ff84c83d258aecebd
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-24 15:25:30 +0400

    Implement HomePage Functionality
     Added main content and redirection functionality to the gallery page.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import HeroImage from '../assets/HeroImage.png';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
-    return <div>HomePage</div>;
+    const navigate = useNavigate();
+    const navigateToGalleryPage = () => navigate('/gallery');
+
+    return (
+        <div>
+            <img src={HeroImage} alt="iPhone 12 Ad Photo" />
+            <button onClick={navigateToGalleryPage}>Go to Gallery</button>
+        </div>
+    );
 }
 
 export default HomePage;
