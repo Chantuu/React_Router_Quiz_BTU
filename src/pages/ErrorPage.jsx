adefacad2ff74a672ed9f51a4d1be5a81a61daf8
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-25 15:06:54 +0400

    Style Error Page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function ErrorPage() {
-    return <div>Error Page</div>;
+    return (
+        <div className="h-full px-20 flex flex-col mt-40">
+            <h1 className="text-center text-5xl text-red-500">Error 404</h1>
+            <p className="text-center mt-2">
+                Unfortunately, requested resource could not be found!
+            </p>
+        </div>
+    );
 }
 
 export default ErrorPage;
