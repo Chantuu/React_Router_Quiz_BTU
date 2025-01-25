3abd32f329a587348e238baadc65445548c8bed2
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-25 13:30:15 +0400

    Make GalleryPage Responsive
     Added new styles to GalleryPage div for making photocards component centered.

--- src/pages/GalleryPage.jsx
+++ src/pages/GalleryPage.jsx
@@ -3,9 +3,9 @@ import PhotoCards from '../components/PhotoCards';
 
 function GalleryPage() {
     return (
-        <>
+        <div className="h-full flex justify-center items-center pt-8 md:pt-0">
             <PhotoCards />
-        </>
+        </div>
     );
 }
 
