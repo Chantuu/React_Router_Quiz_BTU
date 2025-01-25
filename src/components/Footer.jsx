985f68bbffbbf699cc33e0745da93c2af2119fdf
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-25 15:37:50 +0400

    Add Link Styling
     Added proper styling to links in ProfilePage and Footer components.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,12 @@ function Footer() {
     return (
         <footer className="bg-blue-600 px-3 md:px-14 py-3.5 md:py-7 h-1/10">
             <p className="text-white text-center text-xs md:text-sm">
-                This project is made by Giorgi Chanturia.
-                <a href="https://github.com/">
-                    Source Code will be available soon.
+                This project is made by Giorgi Chanturia.{' '}
+                <a
+                    href="https://github.com/Chantuu/React_Router_Quiz_BTU"
+                    className="underline transition-colors delay-100 ease-out hover:text-gray-300"
+                >
+                    Source Code is available here.
                 </a>
             </p>
         </footer>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 
 function ProfilePage() {
     const profileName = useParams().profileName;
+    const linkStyle =
+        'text-blue-500 underline transition-colors delay-100 ease-out hover:text-blue-800';
 
     return (
         <div className="h-full px-20 flex flex-col mt-40">
@@ -11,9 +13,18 @@ function ProfilePage() {
             </h1>
             <p className="text-center mt-4">
                 This project is made by me for React Quiz in BTU University.
-                Source code is available <a>here.</a> Thank you very much for
-                checking out my project. Don't forget to visit other projects{' '}
-                <a href="https://github.com/">over here!</a>
+                Source code is available{' '}
+                <a
+                    href="https://github.com/Chantuu/React_Router_Quiz_BTU"
+                    className={linkStyle}
+                >
+                    here.
+                </a>{' '}
+                Thank you very much for checking out my project. Don't forget to
+                visit other projects{' '}
+                <a href="https://github.com/Chantuu" className={linkStyle}>
+                    over here!
+                </a>
             </p>
         </div>
     );
