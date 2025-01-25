c8c53c11dbef6d9345658bb4aa8777316ff381d7
Giorgi Chanturia <47988187+Chantuu@users.noreply.github.com>
2025-01-25 14:58:57 +0400

    Design Profile Page
     Implemented full responsive design for profile page and ability to display name based on parameters.

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function ProfilePage() {
-    return <div>Profile Page</div>;
+    const profileName = useParams().profileName;
+
+    return (
+        <div className="h-full px-20 flex flex-col mt-40">
+            <h1 className="text-center text-5xl">
+                Profile Name: {profileName || 'Giorgi Chanturia'}
+            </h1>
+            <p className="text-center mt-4">
+                This project is made by me for React Quiz in BTU University.
+                Source code is available <a>here.</a> Thank you very much for
+                checking out my project. Don't forget to visit other projects{' '}
+                <a href="https://github.com/">over here!</a>
+            </p>
+        </div>
+    );
 }
 
 export default ProfilePage;
